3de514d5c1e4816b2283f18a23e74780edd0539f
Mohamed Amgad <m.amgad.bc@gmail.com>
2019-11-24 22:50:48 +0200

    adding gh-pages

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React from 'react';
 import './search-box.styles.css';
 export const SearchBox = ({placeholder, handleChange})=>{
     return <input className="search" type="search" placeholder={placeholder} onChange={handleChange} />;
