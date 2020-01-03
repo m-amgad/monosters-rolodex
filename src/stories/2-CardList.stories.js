d551ec4d0efe4d8b5dce609451a9be329abfbf45
Ahmed Ayoub <great.nile@gmail.com>
2020-01-03 02:19:29 +0100

    use object type for monsters list

--- src/stories/2-CardList.stories.js
+++ src/stories/2-CardList.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CardList } from '../components/card-list/card-list.component';
-import { withKnobs, text, number } from '@storybook/addon-knobs';
+import { withKnobs, number, object } from '@storybook/addon-knobs';
 
 export default {
   title: 'Card List Component',
@@ -9,14 +9,34 @@ export default {
 
 export const toStorybook = () => {
   let containerWidth = number('ContainerWidth', 300);
-  let monstersList = [];
+  let monstersList = []
   return (
-    <div style={{ width: `${containerWidth}px`}}>
+    <div style={{ width: `${containerWidth}px` }}>
       <CardList monsters={monstersList} />
     </div>
   );
 };
 
 toStorybook.story = {
-  name: 'Simple Empty Card List'
+  name: 'empty card list'
+};
+
+
+export const toStorybook2 = () => {
+  let containerWidth = number('ContainerWidth', 300);
+  let monstersList = object('Monsters Array', [
+    { name: 'nile', id: 200, email: 'nile@nile.nile' },
+    { name: 'nile2', id: 100, email: 'nile2@nile.nile' }
+  ]);
+
+
+  return (
+    <div style={{ width: `${containerWidth}px` }}>
+      <CardList monsters={monstersList} />
+    </div>
+  );
+};
+
+toStorybook2.story = {
+  name: 'multiple cards'
 };
